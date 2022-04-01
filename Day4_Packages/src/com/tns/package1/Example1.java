2e3de92532de1448527baa8e50d3f876d4a01e85
Rakesh-P-Rao <rakesh.rao278698@gmail.com>
2022-04-01 18:57:27 +0530

    Day4 done

---
+++ Day4_Packages/src/com/tns/package1/Example1.java
@@ -0,0 +1,8 @@
+package com.tns.package1;
+
+public class Example1 {
+	public void display()
+	{
+		System.out.println("welcome to the concept of packages");
+	}
+}
---
+++ Day4_Packages/src/com/tns/package2/Example2.java
@@ -0,0 +1,11 @@
+package com.tns.package2;
+import com.tns.package1.Example1;
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example1 e=new Example1();
+		e.display();
+	}
+
+}
