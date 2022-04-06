6fd4b0c615fb47c02e9c15ba2589f8a5112a5035
Rakesh-P-Rao <rakesh.rao278698@gmail.com>
2022-04-06 18:36:40 +0530

    Day7 done

---
+++ Day7_this_static_instance_of/src/com/tns/instance1/Example2.java
@@ -0,0 +1,20 @@
+package com.tns.instance1;
+
+class Parent
+{
+	
+}
+class Child extends Parent
+{
+	
+}
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Child c=new Child();
+		System.out.println(c instanceof Parent);
+	}
+
+}
---
+++ Day7_this_static_instance_of/src/com/tns/static1/Example2.java
@@ -0,0 +1,16 @@
+package com.tns.static1;
+
+public class Example2
+{
+	static void print()
+	{
+		System.out.println("Rakesh is doing well in the class");
+	}
+
+	public static void main(String[] args) 
+	{
+		print();
+
+	}
+
+}
